aed659989efb9e9428c704f9217ee91879fd8137
Slava <slaventius2005@gmail.com>
2026-02-14 17:07:15 +0200

    Додано Program.cs з XML-коментарями для документації

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorldApp
+{
+    /// <summary>
+    /// Основна програма, яка демонструє вивід повідомлення на екран.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Точка входу в програму.
+        /// Виводить "Hello World!" у консоль.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
